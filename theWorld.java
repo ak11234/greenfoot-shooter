1cb5ec8d912d9cd40d7a33f10a6c8093f04f8789
ak11234 <sespwalkup@dhcp-129-105-244-217.sesp.northwestern.edu>
2014-11-08 13:13:08 -0600

    Initial Commit
    Has only the world, and a picture.

---
+++ theWorld.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class theWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class theWorld extends World
+{
+
+    /**
+     * Constructor for objects of class theWorld.
+     * 
+     */
+    public theWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+    }
+}
